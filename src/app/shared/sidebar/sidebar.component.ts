f33aa5ee71094f0b6d0b75084c3f26f10a6c6dad
moralitros <danielmoralesmorales1997@gmail.com>
2021-10-26 12:35:14 +0200

    commitGifs

---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+})
+export class SidebarComponent {
+
+  get historial(){
+    return this.gifsService.historial;
+  }
+
+  buscar( item: string){
+
+    return this.gifsService.buscarGifs( item );
+
+  }
+
+  constructor( private gifsService: GifsService) { }
+
+}
